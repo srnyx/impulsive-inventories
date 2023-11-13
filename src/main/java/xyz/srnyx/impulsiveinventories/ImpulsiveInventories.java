24fb16a7a3df4518952a141964ecaa1d2734472d
srnyx <25808801+srnyx@users.noreply.github.com>
2023-11-13 18:55:15 -0500

    Add update platforms

--- src/main/java/xyz/srnyx/impulsiveinventories/ImpulsiveInventories.java
+++ src/main/java/xyz/srnyx/impulsiveinventories/ImpulsiveInventories.java
@@ -1,11 +1,16 @@
 package xyz.srnyx.impulsiveinventories;
 
 import xyz.srnyx.annoyingapi.AnnoyingPlugin;
+import xyz.srnyx.annoyingapi.PluginPlatform;
 
 
 public class ImpulsiveInventories extends AnnoyingPlugin {
     public ImpulsiveInventories() {
         options
+                .pluginOptions(pluginOptions -> pluginOptions.updatePlatforms(
+                        PluginPlatform.modrinth("impulsive-inventories"),
+                        PluginPlatform.hangar(this, "srnyx"),
+                        PluginPlatform.spigot("113437")))
                 .bStatsOptions(bStatsOptions -> bStatsOptions.id(18322))
                 .registrationOptions.toRegister(this, ImpulsiveCommand.class);
     }
